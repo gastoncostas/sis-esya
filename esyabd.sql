a828518c5f29bd2d46deab38b54e9e1062aba5fe
Gastón Costas <gncostas@gmail.com>
2025-08-29 19:13:29 -0300

    borrado de líneas inncesesarias en la base de datos..

--- esyabd.sql
+++ esyabd.sql
@@ -1,27 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Versión del servidor: 9.1.0
--- Versión de PHP: 8.3.14
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
-;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
-;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
-;
-/*!40101 SET NAMES utf8mb4 */
-;
---
--- Base de datos: `esyabd`
---
--- --------------------------------------------------------
---
--- Estructura de tabla para la tabla `asistencia`
---
+
 DROP TABLE IF EXISTS `asistencia`;
 CREATE TABLE IF NOT EXISTS `asistencia` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -2157,18 +2137,6 @@ VALUES (
     '2025-08-29 15:08:57',
     '2025-08-21 19:30:28'
   );
---
--- Restricciones para tablas volcadas
---
---
--- Filtros para la tabla `asistencia`
---
 ALTER TABLE `asistencia`
 ADD CONSTRAINT `asistencia_ibfk_1` FOREIGN KEY (`aspirante_id`) REFERENCES `cursante` (`id`) ON DELETE CASCADE;
 COMMIT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
-;
